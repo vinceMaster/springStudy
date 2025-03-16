a89e6eec17f574f765a1ccd33f2a18887661b05c
seonbin <seonbin2748@aekyung.kr>
2025-03-17 08:56:59 +0900

    20250317/임선빈/spring으로 전환

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -5,15 +5,21 @@ import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
 
+        /*
         AppConfig appConfig = new AppConfig();
-
         MemberService memberService = appConfig.memberService();
         OrderService orderService = appConfig.orderService();
+        */
 
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
         Long memberId = 1l;
         Member member = new Member(memberId, "memberA", Grade.VIP);
